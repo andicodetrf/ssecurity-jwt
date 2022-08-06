ecb6c55afb38ec91d11ef965ea029f09b050b31b
andicodetrf <andrea_lau92@hotmail.com>
2022-08-06 12:12:29 +0800

    add controllers and seed data config

--- src/main/java/com/andi/userservice/UserserviceApplication.java
+++ src/main/java/com/andi/userservice/UserserviceApplication.java
@@ -1,7 +1,15 @@
 package com.andi.userservice;
 
+import java.util.ArrayList;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.andi.userservice.domain.Role;
+import com.andi.userservice.domain.User;
+import com.andi.userservice.service.UserService;
 
 @SpringBootApplication
 public class UserserviceApplication {
@@ -9,5 +17,47 @@ public class UserserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserserviceApplication.class, args);
 	}
+	
+	//mock/seed data
+	//add Bean annotation otherwise wont get picked up by Spring when app runs
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		//will run after the application has initialized
+		return args -> {
+			//1st arg is id - unnecessary to pass as Jpa will insert automatically, hence null
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+			
+			
+			userService.saveUser(new User(null, "Brad Pitt", "brad", "BD123", new ArrayList<>()));
+			userService.saveUser(new User(null, "John Depp", "john", "JD123", new ArrayList<>()));
+			userService.saveUser(new User(null, "Chris Hemsworth", "chris", "CH123", new ArrayList<>()));
+			userService.saveUser(new User(null, "Adam Sandler", "adam", "AS123", new ArrayList<>()));
+			
+			
+			userService.addRoleToUser("brad", "ROLE_MANAGER");
+			
+			userService.addRoleToUser("john", "ROLE_ADMIN");
+			
+			userService.addRoleToUser("adam", "ROLE_USER");
+			userService.addRoleToUser("adam", "ROLE_MANAGER");
+			
+			userService.addRoleToUser("chris", "ROLE_SUPER_ADMIN");
+			userService.addRoleToUser("chris", "ROLE_ADMIN");
+			userService.addRoleToUser("chris", "ROLE_USER");
+		};
+	}
 
 }
+
+//api/users getUsers
+//[
+// {"id":5,"name":"Brad Pitt","username":"brad","password":"BD123","roles":[{"id":2,"name":"ROLE_MANAGER"}]},
+// {"id":6,"name":"John Depp","username":"john","password":"JD123","roles":[{"id":3,"name":"ROLE_ADMIN"}]},
+// {"id":7,"name":"Chris Hemsworth","username":"chris","password":"CH123","roles":[{"id":4,"name":"ROLE_SUPER_ADMIN"},{"id":3,"name":"ROLE_ADMIN"},{"id":1,"name":"ROLE_USER"}]},
+// {"id":8,"name":"Adam Sandler","username":"adam","password":"AS123","roles":[{"id":1,"name":"ROLE_USER"},{"id":2,"name":"ROLE_MANAGER"}]}
+//]
+		
+		
--- src/main/java/com/andi/userservice/api/UserResource.java
+++ src/main/java/com/andi/userservice/api/UserResource.java
@@ -1,16 +1,25 @@
 package com.andi.userservice.api;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.andi.userservice.domain.Role;
 import com.andi.userservice.domain.User;
 import com.andi.userservice.service.UserService;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+
+
+//use postman to pass the data or config some data whenever app start (see App entry) 
 
 @RestController
 @RequestMapping("/api")
@@ -18,10 +27,44 @@ import lombok.RequiredArgsConstructor;
 public class UserResource {
 	private final UserService userService;
 	
-	
 	@GetMapping("/users")
-	public ResponseEntity<List<User>>getUsers(){
+	public ResponseEntity<List<User>>apiGetUsers(){
+		//.ok() = 200
 		return ResponseEntity.ok().body(userService.getUsers());
-		
 	}
+	
+	@PostMapping("/user/save")
+	public ResponseEntity<User>apiSaveUser(@RequestBody User user){
+		//.created() = 201		
+		//can create whatever string or retrieve from current path - localhost8080/api/user/save - then turn it into uri string.
+		URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+		//.created takes in a uri string
+		//spring will take this uri and place it in the headers
+		return ResponseEntity.created(uri).body(userService.saveUser(user));
+	}
+	
+	@PostMapping("/role/save")
+	public ResponseEntity<Role>apiSaveRole(@RequestBody Role role){
+		URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+		return ResponseEntity.created(uri).body(userService.saveRole(role));
+	}
+	
+	//can make ResponseEntity to return whatever you want
+	//but this method is going to return void so can use ? in ResponseEntity
+	@PostMapping("/role/addtouser")
+	public ResponseEntity<?>apiAddRoleToUser(@RequestBody RoleToUserForm form){
+		//need a way to get the name and role from request to pass into addRoleToUser service - via form
+		userService.addRoleToUser(form.getUsername(), form.getRoleName());
+		
+		//since not returning anything, just return ok to client
+		return ResponseEntity.ok().build();
+	}
+}
+
+
+@Data
+class RoleToUserForm {
+	private String username;
+	private String roleName;
+	
 }
